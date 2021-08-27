f41da2e28668be61bc1830b496843cb56ec81cf3
miaanderson <andersmi24@up.edu>
2021-08-26 18:17:19 -0700

    Two candles evenly spaced

--- app/src/main/java/cs301/birthdaycake/CakeView.java
+++ app/src/main/java/cs301/birthdaycake/CakeView.java
@@ -121,7 +121,7 @@ public class CakeView extends SurfaceView {
 
         //Now a candle in the center
         drawCandle(canvas, cakeLeft + cakeWidth/3 - candleWidth/2, cakeTop);
-        drawCandle(canvas, cakeLeft + cakeWidth*(2/3) - candleWidth/2, cakeTop);
+        drawCandle(canvas, cakeLeft + cakeWidth*2/3 - candleWidth/2, cakeTop);
     }//onDraw
 
 }//class CakeView
